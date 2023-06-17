4bb9d5840a46021e9fed3b21415b8a403bf93e08
charmingdata <94773218+charmingdata@users.noreply.github.com>
2023-06-17 11:09:39 -0400

    Update hardhat.config.js

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -5,7 +5,7 @@ const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.18",
   networks: {
     sepolia: {
       url: QUICKNODE_HTTP_URL,
