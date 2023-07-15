a80289786f8eee63fc9e3a186b7c16c1375c2388
charmingdata <94773218+charmingdata@users.noreply.github.com>
2023-07-14 20:32:57 -0400

    Update hardhat.config.js

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,14 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env" });
 
-const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
   networks: {
-    sepolia: {
-      url: QUICKNODE_HTTP_URL,
+    zkEVM: {
+      url: `https://rpc.public.zkevm-test.net`,
       accounts: [PRIVATE_KEY],
     },
   },
